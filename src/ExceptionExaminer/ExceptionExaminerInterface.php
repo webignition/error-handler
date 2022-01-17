01a2b6e7fec2f5b19823a4230d0d361f4ca73cd5
Jon Cram <webignition@gmail.com>
2022-01-17 17:27:38 +0000

    Use php-cs-fixer

--- src/ExceptionExaminer/ExceptionExaminerInterface.php
+++ src/ExceptionExaminer/ExceptionExaminerInterface.php
@@ -7,6 +7,8 @@ namespace webignition\ErrorHandler\ExceptionExaminer;
 interface ExceptionExaminerInterface
 {
     public function isFatal(\Exception $exception): bool;
+
     public function isIgnored(\Exception $exception): bool;
+
     public function isRecoverable(\Exception $exception): bool;
 }
